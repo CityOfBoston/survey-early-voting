7505b912bb134b0928fa010c87cc84773a91aa24
Matthew Crist <matthew.crist@boston.gov>
2016-06-20 14:30:21 -0400

    Updated dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,17 @@
+require 'csv'
+
 class DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def index
     @locations = Location.all.order('votes_count DESC')
+
+    respond_to do |format|
+      format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"user-list\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+    end
   end
 end
